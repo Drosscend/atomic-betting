8ebd137839a3d1a854159498fb61694c4e6db7c0
Drosscend <47970260+Drosscend@users.noreply.github.com>
2024-09-11 09:27:42 +0200

    Chore(SpeedInsights): add vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <SpeedInsights />
         {children}
       </body>
     </html>
